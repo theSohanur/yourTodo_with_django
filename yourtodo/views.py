b68454abdb0b2c764b48be88c7ce17a7f871d04e
Sohanur Rahman <sohanur064@gmail.com>
2021-03-23 23:57:51 +0600

    redirect with signup with err handeling

--- yourtodo/views.py
+++ yourtodo/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.db import IntegrityError
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import login
+
 
 # Create your views here.
 
@@ -9,10 +12,19 @@ def signupuser(request):
         return render(request, 'yourtodo/signup.html', {'form': UserCreationForm()})
     else:
         if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
-            user.save()
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                redirect('currenttodos')
+            except IntegrityError:
+                return render(request, 'yourtodo/signup.html',
+                              {'form': UserCreationForm(), 'error': 'That username has already has taken, Please '
+                                                                    'choose a new Username.'})
         else:
-            print('hello')
+            return render(request, 'yourtodo/signup.html',
+                          {'form': UserCreationForm(), 'error': 'Password did not Match'})
 
 
-
+def currenttodos(request):
+    return render(request,'yourtodo/currenttodos.html')
