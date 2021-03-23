065dce7eeab2d95c70472528640e15723e843c41
Sohanur Rahman <sohanur064@gmail.com>
2021-03-23 23:22:17 +0600

    sign up form

--- yourtodo/views.py
+++ yourtodo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
+
+def signupuser(request):
+    return render(request, 'yourtodo/signup.html', {'form': UserCreationForm()})
